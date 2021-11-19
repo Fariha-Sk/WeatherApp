19060a471d9497995614caa1c0691f3d7259f6ec
Fariha Shaikh <fariha7264@gmail.com>
2021-11-20 00:10:34 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import{HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NewComponent } from './new/new.component';
+// import{AgmCoreModule} from '@agm/core';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // AgmCoreModule.forRoot({
+    //   apiKey:'AIzaSyCvRJAYOt9Iucky9EDIGWxXRnmR1c-75Lc'
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
